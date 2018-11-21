49b7c5299e7467248193fdee0ec206190c00577b
DJDomingo <djdomingo71@gmail.com>
2018-11-20 16:04:03 -0800

    Frustrum Calls
    Worked on the frustrum planes.  Unsure if formula is exact.

--- Project1/486_proj1_Source.cpp
+++ Project1/486_proj1_Source.cpp
@@ -75,7 +75,7 @@ int main()
 	int meh; //just used to pause before exiting
 
 	Camera main_cam;
-
+	main_cam.eye = (0, 0, 5);
 
 
 	//view frustrum (6 panels, 8 points)
@@ -98,19 +98,19 @@ void calcNearFrustrum(Camera cam) {
 
 	// Vbl = E + dmin*D + umin*U + rmin*R
 	cam.frust_Vbl = (cam.eye + cam.min_sub_pt*temp_D - (cam.v_height / 2)*temp_U - (cam.v_width / 2)*temp_R);
-	cout << cam.frust_Vbl << endl;
+	cout << "Vbl: " << cam.frust_Vbl << endl;
 
 	// Vtl = E + dmin*D + umax*U + rmin*R
 	cam.frust_Vtl = (cam.eye + cam.min_sub_pt*temp_D + (cam.v_height / 2)*temp_U - (cam.v_width / 2)*temp_R);
-	cout << cam.frust_Vtl << endl;
+	cout << "Vtl: " << cam.frust_Vtl << endl;
 
 	// Vbr = E + dmin*D + umin*U + rmax*R
 	cam.frust_Vbr = (cam.eye + cam.min_sub_pt*temp_D - (cam.v_height / 2)*temp_U + (cam.v_width / 2)*temp_R);
-	cout << cam.frust_Vbr << endl;
+	cout << "Vbr: " << cam.frust_Vbr << endl;
 
 	// Vtr = E + dmin*D + umax*U + rmax*R
 	cam.frust_Vtr = (cam.eye + cam.min_sub_pt*temp_D + (cam.v_height / 2)*temp_U + (cam.v_width / 2)*temp_R);
-	cout << cam.frust_Vtr << endl;
+	cout << "Vtr: " << cam.frust_Vtr << endl;
 }
 
 void calcFarFrustrum(Camera cam) {
@@ -123,19 +123,19 @@ void calcFarFrustrum(Camera cam) {
 
 	// Wbl = E + dmax/dmin(dmin*D + umin*U + rmin*R)
 	cam.frust_Wbl = (cam.eye + temp_dist*(cam.min_sub_pt*temp_D - (cam.v_height / 2)*temp_U - (cam.v_width / 2)*temp_R));
-	cout << cam.frust_Wbl << endl;
+	cout << "Wbl: " << cam.frust_Wbl << endl;
 
 	// Wtl = E + dmin*D + umax*U + rmin*R
 	cam.frust_Wtl = (cam.eye + temp_dist*(cam.min_sub_pt*temp_D + (cam.v_height / 2)*temp_U - (cam.v_width / 2)*temp_R));
-	cout << cam.frust_Wtl << endl;
+	cout << "Wtl: " << cam.frust_Wtl << endl;
 
 	// Wbr = E + dmin*D + umin*U + rmax*R
 	cam.frust_Wbr = (cam.eye + temp_dist*(cam.min_sub_pt*temp_D - (cam.v_height / 2)*temp_U + (cam.v_width / 2)*temp_R));
-	cout << cam.frust_Wbr << endl;
+	cout << "Wbr: " << cam.frust_Wbr << endl;
 
 	// Wtr = E + dmin*D + umax*U + rmax*R
 	cam.frust_Wtr = (cam.eye + temp_dist*(cam.min_sub_pt*temp_D + (cam.v_height / 2)*temp_U + (cam.v_width / 2)*temp_R));
-	cout << cam.frust_Wtr << endl;
+	cout << "Wtr: " << cam.frust_Wtr << endl;
 }
 
 void calcFrustPlanes(Camera cam) {
@@ -144,20 +144,30 @@ void calcFrustPlanes(Camera cam) {
 	gfx::Vector temp_U(0, 1, 0);
 	gfx::Vector temp_R(1, 0, 0);
 	cout << "Frustrum Planes:\n";
+
 	// NearPlane D.E + dmin
-	cam.frust_Nn.Dot(temp_D, cam.eye) + cam.min_sub_pt;
-	cout << cam.frust_Nn << endl;
+	cam.frust_Nn = cam.frust_Nn.Dot(temp_D, cam.eye) + cam.min_sub_pt;
+	cout << "Nn: " << cam.frust_Nn << endl;
 
 	// FarPlane
-	cam.frust_Nf.Dot(temp_D, cam.eye) + cam.min_sub_pt;
-	cam.frust_Nf = -cam.frust_Nf;
-	cout << cam.frust_Nf << endl;
+	cam.frust_Nf = -(cam.frust_Nf.Dot(temp_D, cam.eye) + cam.min_sub_pt);
+	cout << "Nf: " << cam.frust_Nf << endl;
 
 	// LeftPlane
+	cam.frust_Nl = (cam.min_sub_pt*temp_R - (-cam.v_width/2) *temp_D) * (1/sqrt(cam.min_sub_pt*cam.min_sub_pt + (cam.v_width / 2)*(cam.v_width / 2)));
+	//cam.frust_Nl = (cam.v_height)*(cam.min_sub_pt*temp_R - cam.v_width*temp_D);
+	cout << "Nl: " << cam.frust_Nl << endl;
+
 	// RightPlane
+	cam.frust_Nr = (-cam.min_sub_pt*temp_R + (cam.v_width / 2) *temp_D) * (1 / sqrt(cam.min_sub_pt*cam.min_sub_pt + (-cam.v_width / 2)*(-cam.v_width / 2)));
+	cout << "Nr: " << cam.frust_Nr << endl;
 	// BotPlane
+	cam.frust_Nb = (cam.min_sub_pt*temp_U - (-cam.v_width / 2) *temp_D) * (1 / sqrt(-cam.min_sub_pt*-cam.min_sub_pt + (-cam.v_width / 2)*(-cam.v_width / 2)));
+	cout << "Nb: " << cam.frust_Nb << endl;
 	// TopPlane
-}
+	cam.frust_Nt = (-cam.min_sub_pt*temp_U + (cam.v_width / 2) *temp_D) * (1 / sqrt(-cam.min_sub_pt*-cam.min_sub_pt + (cam.v_width / 2)*(cam.v_width / 2)));
+	cout << "Nt: " << cam.frust_Nt << endl;
+}	
 
 void vectMult(float vect[], float val) {
 	for (int i = 0; i < 3; i++) {
