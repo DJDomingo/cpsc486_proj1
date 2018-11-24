10bd3d2d21197be7ebbb4066597ec2de175f4c7e
CS254 <sersafir@gmail.com>
2018-11-24 14:23:58 -0800

    Add files via upload
    Merged my scan with the project so we can determine the input values.

--- Project1/486_proj1_Source.cpp
+++ Project1/486_proj1_Source.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
-#include <string>
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<vector>
+#include<algorithm>
+#include<math.h>
 #include "math.h"
+
+using namespace std;
+
+//functions made
+void pause();
+vector<double> returnNumbers(int i, string); //returns numbers from a line within a string.
+int returnDigit(char digit);
+struct output
+{
+	string description;
+	vector<double> data;
+};
+output scan(string);
 using namespace std;
 
 
@@ -43,6 +60,45 @@ void calcFrustPlanes(Camera);
 void printTri(Triangle);
 int main()
 {
+	ifstream myfile("test.txt");
+	if (myfile.fail())
+	{
+		cout << "\n** ERROR - the file cannot be found! One has been created. Please place the data in test.txt";
+		ofstream myfile3("test.txt");
+		pause();
+		return 0;
+	}
+	else {
+		cout << "The file test.txt is found.\nlooking through data.\n";
+		string line;
+		vector<output> vec;
+		vector<output> determineVector;
+		while (getline(myfile, line))
+		{
+
+			if (scan(line).description == "NULL")
+			{
+				for (int i = 0; i < scan(line).data.size(); i++)
+				{
+					//cout << scan(line).data.at(i) << endl;
+					vec.at(vec.size() - 1).data.push_back(scan(line).data.at(i));
+				}
+			}
+			else
+				vec.push_back(scan(line));
+		}
+		cout << "vector of size : " << vec.size() << endl;
+		for (int i = 0; i < vec.size(); i++)
+		{
+			cout << vec.at(i).description << endl;
+			for (int j = 0; j < vec.at(i).data.size(); j++)
+			{
+				cout << vec.at(i).data.at(j) << endl;
+			}
+		}
+		pause();
+
+	}
 	//input should be: Model Space, World Space, and ID of cam
 	int clip_cam = 9; //cam to clip against
 	int meh; //just used to pause before exiting
@@ -57,15 +113,15 @@ int main()
 	printTri(test);
 
 	// 1. Convert to world space from model space
-		//ex) try loc(-70,0,80) with az -90 and elv -30
-	toWorldSpace(test, gfx::Vector(-70,0,80), -90, -30);
+	//ex) try loc(-70,0,80) with az -90 and elv -30
+	toWorldSpace(test, gfx::Vector(-70, 0, 80), -90, -30);
 
 	// 2. Calculate view frustrum (6 panels, 8 points)
 	calcNearFrustrum(main_cam); // near frustrum vertices
 	calcFarFrustrum(main_cam); // far frustrum vertices
 	calcFrustPlanes(main_cam); // frustrum planes
 
-	// 3. Cull/Clip: Check if triangles fall in, out, or partial of frustrum.
+							   // 3. Cull/Clip: Check if triangles fall in, out, or partial of frustrum.
 
 
 	cin >> meh;
@@ -111,19 +167,19 @@ void calcFarFrustrum(Camera cam) {
 	cout << "Far Frustrums:\n";
 
 	// Wbl = E + dmax/dmin(dmin*D + umin*U + rmin*R)
-	cam.frust_Wbl = (cam.eye + temp_dist*(cam.min_sub_pt*temp_D - (cam.v_height / 2)*temp_U - (cam.v_width / 2)*temp_R));
+	cam.frust_Wbl = (cam.eye + temp_dist * (cam.min_sub_pt*temp_D - (cam.v_height / 2)*temp_U - (cam.v_width / 2)*temp_R));
 	cout << "Wbl: " << cam.frust_Wbl << endl;
 
 	// Wtl = E + dmin*D + umax*U + rmin*R
-	cam.frust_Wtl = (cam.eye + temp_dist*(cam.min_sub_pt*temp_D + (cam.v_height / 2)*temp_U - (cam.v_width / 2)*temp_R));
+	cam.frust_Wtl = (cam.eye + temp_dist * (cam.min_sub_pt*temp_D + (cam.v_height / 2)*temp_U - (cam.v_width / 2)*temp_R));
 	cout << "Wtl: " << cam.frust_Wtl << endl;
 
 	// Wbr = E + dmin*D + umin*U + rmax*R
-	cam.frust_Wbr = (cam.eye + temp_dist*(cam.min_sub_pt*temp_D - (cam.v_height / 2)*temp_U + (cam.v_width / 2)*temp_R));
+	cam.frust_Wbr = (cam.eye + temp_dist * (cam.min_sub_pt*temp_D - (cam.v_height / 2)*temp_U + (cam.v_width / 2)*temp_R));
 	cout << "Wbr: " << cam.frust_Wbr << endl;
 
 	// Wtr = E + dmin*D + umax*U + rmax*R
-	cam.frust_Wtr = (cam.eye + temp_dist*(cam.min_sub_pt*temp_D + (cam.v_height / 2)*temp_U + (cam.v_width / 2)*temp_R));
+	cam.frust_Wtr = (cam.eye + temp_dist * (cam.min_sub_pt*temp_D + (cam.v_height / 2)*temp_U + (cam.v_width / 2)*temp_R));
 	cout << "Wtr: " << cam.frust_Wtr << endl;
 
 	cout << endl;
@@ -145,7 +201,7 @@ void calcFrustPlanes(Camera cam) {
 	cout << "Nf: " << cam.frust_Nf << endl;
 
 	// LeftPlane
-	cam.frust_Nl = (cam.min_sub_pt*temp_R - (-cam.v_width/2) *temp_D) * (1/sqrt(cam.min_sub_pt*cam.min_sub_pt + (cam.v_width / 2)*(cam.v_width / 2)));
+	cam.frust_Nl = (cam.min_sub_pt*temp_R - (-cam.v_width / 2) *temp_D) * (1 / sqrt(cam.min_sub_pt*cam.min_sub_pt + (cam.v_width / 2)*(cam.v_width / 2)));
 	//cam.frust_Nl = (cam.v_height)*(cam.min_sub_pt*temp_R - cam.v_width*temp_D);
 	cout << "Nl: " << cam.frust_Nl << endl;
 
@@ -167,4 +223,215 @@ void printTri(Triangle tri) {
 	for (int i = 0; i < 3; i++) {
 		cout << "point " << i << ": " << tri.pts[i] << endl;
 	}
+}
+
+
+
+void pause() {//simple pausing function for readability
+	string junk;
+	getline(cin, junk);
+}
+output scan(string line)
+{
+	output result;
+	result.description = "NULL";
+	vector<double> info;
+
+	//we know it's not a comment
+	//line.erase(remove_if(line.begin(), line.end(), isspace), line.end());
+	//Does this describe the camera?
+	for (int i = 0; i < line.length(); i++)
+	{
+		if (line.at(i) == 'c' && (i + 6) < line.length()) {
+			if (line.at(i + 1) == 'a')
+				if (line.at(i + 2) == 'm')
+					if (line.at(i + 3) == 'e')
+						if (line.at(i + 4) == 'r')
+							if (line.at(i + 5) == 'a')
+							{
+								//line is about camera
+								i += 5;
+								result.description = "Camera space";
+								//now we seek 3 numbers associated with camera
+								info = returnNumbers(i, line);
+								result.data = info;
+							}
+		}
+		else if (line.at(i) == 't' && (i + 2) < line.length()) {
+			if (line.at(i + 1) == 'r')
+				if (line.at(i + 2) == 'i')
+				{
+					result.description = "Tri";
+				}
+			//The first part takes in the keyword camera, then adds all numbers to that specific vector, then places them within an output value.
+
+		}
+		else if (line.at(i) == 'b' && (i + 2) < line.length()) {
+			if (line.at(i + 1) == 'a')
+				if (line.at(i + 2) == 'g')
+				{
+					result.description = "Bag";
+					info = returnNumbers(i, line);
+					result.data = info;
+				}
+		}
+		else if (line.at(i) == 'l' && (i + 2) < line.length()) {
+			if (line.at(i + 1) == 'o')
+				if (line.at(i + 2) == 'c')
+				{
+					bool twoDigit = (returnDigit(line.at(i + 5)) >= 0);
+					string firstNum(1, line.at(i + 4));
+					string secondNum(1, line.at(i + 5));
+					if (twoDigit)
+					{
+						result.description = "kid-loc " + firstNum + secondNum;
+						info = returnNumbers(i + 6, line);
+						result.data = info;
+
+					}
+					else {
+						result.description = "kid-loc " + firstNum;
+						info = returnNumbers(i + 5, line);
+						result.data = info;
+					}
+				}
+		}
+
+	}
+	if (result.description == "NULL")
+	{
+		info = returnNumbers(2, line);
+		result.data = info;
+	}
+	return result;
+
+}
+
+vector<double> returnNumbers(int start, string line)
+{
+	vector<double> returnVal;
+	//state1, not number
+	bool state1 = false;
+	//state2, number
+	bool state2 = false;
+	//state3, decimal
+	bool state3 = false;
+	double currentNum = 0.0;
+	double decimalAdd = 0.0;
+	bool addZero = false;
+	bool negativeNumber = false;
+	bool zeroFound = false;
+	for (int i = start; i < line.length(); i++)
+	{
+		if (returnDigit(line.at(i)) == 0 && state1)
+		{
+			zeroFound = true;
+		}
+		if (returnDigit(line.at(i)) == -2 && zeroFound == true)
+		{
+			zeroFound = false;
+		}
+		else if (addZero)
+		{
+			returnVal.push_back(0.0);
+			state1 = true;
+			state2 = false;
+			state3 = false;
+			addZero = false;
+		}
+		else if (returnDigit(line.at(i)) == -3)
+		{
+			state1 = true;
+			state2 = false;
+			state3 = false;
+			negativeNumber = true;
+
+		}
+		else if (currentNum != 0.0 && returnDigit(line.at(i)) == -1) //we have a full number
+		{
+			if (negativeNumber) {
+				currentNum = currentNum * -1;
+				negativeNumber = false;
+			}
+			returnVal.push_back(currentNum);
+			state1 = true;
+			state2 = false;
+			state3 = false;
+			currentNum = 0.0;
+			decimalAdd = 0.0;
+		}
+		else if (returnDigit(line.at(i)) == -1)
+		{
+			if (zeroFound)
+			{
+				returnVal.push_back(0);
+				zeroFound = false;
+			}
+			state1 = true;
+			state2 = false;
+			state3 = false;
+		}
+		else if (returnDigit(line.at(i)) == -2 && state2 == true)
+		{
+			state1 = false;
+			state2 = false;
+			state3 = true;
+		}
+		else
+		{
+			state1 = false;
+			state2 = true;
+			state3 = false;
+		}
+		//for state 1, just wait.
+		//for state 2, we have a number and must now create it.
+		if (state2)
+		{
+			while (returnDigit(line.at(i)) >= 0)
+			{
+				currentNum = currentNum * 10;
+				currentNum += returnDigit(line.at(i));
+				i++;
+			}
+			i--;
+		}
+		if (state3)
+		{
+			i += 1;
+			int countDigits = 0;
+			while (returnDigit(line.at(i)) >= 0) {
+				decimalAdd = decimalAdd * 10;
+				decimalAdd += returnDigit(line.at(i));
+				i++;
+				countDigits++;
+			}
+
+			double divisor = pow(10, countDigits);
+			decimalAdd = decimalAdd / divisor;
+			currentNum = currentNum + decimalAdd;
+			if (currentNum == 0.0)
+				addZero = true;
+			i--;
+		}
+
+	}
+	return returnVal;
+}
+
+int returnDigit(char digit)
+{
+	int returnVal = -1;
+	if (digit == '0' || digit == '1' || digit == '2' || digit == '3' || digit == '4' || digit == '5' || digit == '6' || digit == '7' || digit == '8' || digit == '9')
+	{
+		returnVal = (int)digit - 48;
+	}
+	else if (digit == '.')
+	{
+		returnVal = -2;
+	}
+	else if (digit == '-')
+	{
+		returnVal = -3;
+	}
+	return returnVal;
 }
